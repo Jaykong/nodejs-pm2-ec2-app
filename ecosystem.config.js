638bc4a0f6bf3b440e96f5d9deb7f700a3ea86ac
home_iMac <kongyunpeng2011@sina.com>
2019-01-29 02:05:33 +0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-3-17-39-8.us-east-2.compute.amazonaws.com",
+      host: "ec2-3-17-39-61.us-east-2.compute.amazonaws.com",
       key: "~/.ssh/home.pem",
       ref: "origin/master",
       repo: "git@github.com:Jaykong/nodejs-pm2-ec2-app.git",
